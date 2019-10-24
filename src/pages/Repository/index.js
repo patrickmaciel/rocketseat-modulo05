6458cb2b5922765111884f0a1665c06dfde4f624
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-24 06:55:37 -0300

    carregando os dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { promises } from 'dns';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    /**
+     * executing that way, the second request need to wait the first request
+     * to finish, then the second start */
+    // const response = await api.get(`/repos/${repoName}`);
+    // const response = await api.get(`/repos/${repoName}/issues`);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`),
+      {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      },
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>Repository</h1>;
+  }
 }
